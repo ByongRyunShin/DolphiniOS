cedb9b7b01579806beaedc450b0fd49dd8326f60
신병륜 <br.shin@koscom.co.kr>
2022-04-17 18:05:54 +0900

    이미지추가
    이미지추가

--- DolphiniOS/ContentView.swift
+++ DolphiniOS/ContentView.swift
@@ -10,7 +10,9 @@ import WebKit
 
 struct ContentView: View {
     var body: some View {
-        Webview(url: URL(string: "http://222.122.196.22:8380/login.frm")!)
+            Image("logo")
+            Webview(url: URL(string: "http://222.122.196.22:8380/login.frm")!)
+        
     }
 }
 
@@ -25,5 +27,22 @@ struct Webview: UIViewRepresentable {
     func updateUIView(_ webview: WKWebView, context: UIViewRepresentableContext<Webview>) {
         let request = URLRequest(url: self.url, cachePolicy: .returnCacheDataElseLoad)
         webview.load(request)
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        let url = webView.url
+
+        if url?.absoluteString.range(of: ".pdf") != nil {
+            pdfBackButton.isHidden = false
+        }
+        else {
+            pdfBackButton.isHidden = true
+        }
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View{
+        ContentView()
     }
 }
